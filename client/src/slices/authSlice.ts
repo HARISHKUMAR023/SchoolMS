ece46351e8613ba6d45679b86056846dfae1055f
Harish <harishkumarsp023@gmail.com>
2024-06-25 11:15:19 +0530

    chore: Add lazy loading for images and update redux-persist configuration

--- client/src/slices/authSlice.ts
+++ client/src/slices/authSlice.ts
@@ -1,17 +1,39 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { jwtDecode } from "jwt-decode";
+// Define possible roles
+type Role = 'admin' | 'teacher' | 'student';
+
+interface User {
+  iss?: string;
+  sub?: string;
+  aud?: string | string[];
+  exp?: number;
+  nbf?: number;
+  iat?: number;
+  jti?: string;
+  role: Role; // Use the Role type here
+}
+
+interface AuthState {
+  token: string | null;
+  user: User | null;
+}
+
+const initialState: AuthState = {
+  token: null,
+  user: null,
+};
 
 const authSlice = createSlice({
   name: 'auth',
-  initialState: {
-    token: null,
-    user: null, // This should include role
-  },
+  initialState,
   reducers: {
-    login(state, action) {
+    login(state, action: PayloadAction<{ token: string }>) {
       state.token = action.payload.token;
-      const decodedToken = jwtDecode(action.payload.token);
-      state.user = { role: decodedToken.role, ...decodedToken };
+      const decodedToken = jwtDecode<User>(action.payload.token);
+      // If you want to ensure `role` is set and then spread the rest of `decodedToken`, ensure `role` is set last.
+      // This example assumes `role` is always present in `decodedToken`.
+      state.user = { ...decodedToken, role: decodedToken.role };
     },
     logout(state) {
       state.token = null;
